5d05abf4e86f60e9ccfdee7f732f5179cbb867ac
root <root@localhost.(none)>
2015-11-13 12:01:38 +0800

    增加ll脚本

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ echo '/usr/bin/zyshell' >>/etc/shells
 \cp -pr ./zyshell.conf  /etc/zyshell.conf 
 \cp -pr ./zyshell /usr/bin/
 \cp -pr zyshell.py   /usr/lib/python2.6/site-packages/zyshell.py
+\cp -pr ll  /sbin/
 chsh -s /usr/bin/zyshell  web
